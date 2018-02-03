b58c46b5b16abeedbe142c478e03cf615359a09c
fjayblue <fjayblue@126.com>
2018-02-03 15:08:20 +0800

    调整：去除无用包

--- src/main/db/db.sql
+++ src/main/db/db.sql
@@ -8,21 +8,21 @@ CREATE TABLE system_config
 (
   id          BIGINT UNSIGNED AUTO_INCREMENT
   COMMENT '自增长标识',
-  is_enabled  SMALLINT DEFAULT 1                                                 NOT NULL
+  is_enabled  TINYINT DEFAULT 1                                                   NOT NULL
   COMMENT '是否开启',
-  type        VARCHAR(32) DEFAULT ''                                             NOT NULL
+  type        VARCHAR(32) DEFAULT ''                                              NOT NULL
   COMMENT '类型',
-  name        VARCHAR(255) DEFAULT ''                                            NOT NULL
+  name        VARCHAR(255) DEFAULT ''                                             NOT NULL
   COMMENT '键',
-  value       VARCHAR(4000) DEFAULT ''                                           NOT NULL
+  value       VARCHAR(4000) DEFAULT ''                                            NOT NULL
   COMMENT '值',
-  sequence_no BIGINT DEFAULT 0                                                   NOT NULL
+  sequence_no BIGINT DEFAULT 0                                                    NOT NULL
   COMMENT '顺序号',
-  description VARCHAR(255) DEFAULT ''                                            NOT NULL
+  description VARCHAR(255) DEFAULT ''                                             NOT NULL
   COMMENT '描述',
-  create_time DATETIME DEFAULT CURRENT_TIMESTAMP                                 NOT NULL
+  create_time TIMESTAMP DEFAULT CURRENT_TIMESTAMP                                 NOT NULL
   COMMENT '创建时间',
-  update_time DATETIME ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP     NOT NULL
+  update_time TIMESTAMP ON UPDATE CURRENT_TIMESTAMP DEFAULT CURRENT_TIMESTAMP     NOT NULL
   COMMENT '更新时间',
   PRIMARY KEY (id)
 )
